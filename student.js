90f566d31a2306f2fb5ac117ff0ce756a110d3a8
jms-1005 <96402058+jms-1005@users.noreply.github.com>
2022-02-18 18:32:31 -0800

    Add files via upload
    CRUD

--- student.js
+++ student.js
@@ -32,22 +32,35 @@ function displayStudents(){
     let _student = '';
     $('.students').html('');
     for(let i=0; i<students.length; i++){
-       
+       console.log(students[i].deleted)
+        if(students[i].deleted !== true){
+            _student += `<div class="row student" id="s${students[i].id}">
+                <div class="col-lg-2 image">
+                    <img src="${students[i].image}">
+                </div>
+                <div class="col-lg-10">
+                    <h3 class="name">${ students[i].name }</h3>
+                    <div class="email">Email: <a href="mailto:${ students[i].email }">${ students[i].email }</a></div>
+                    <div class="id">ID: ${ students[i].id }</div>
 
-        _student += `<div class="row student">
-            <div class="col-lg-2 image">
-                <img src="${students[i].image}">
-            </div>
-            <div class="col-lg-10">
-                <h3 class="name">${ students[i].name }</h3>
-                <div class="email">${ students[i].email }</div>
-                <div class="id">${ students[i].id }</div>
-                <div class="admissionStatus">Admission Status: <input type="checkbox" ></div>
-                <div class="deleted">Delete Student: <input type="checkbox"></div>
-            </div>
-        </div>`
-        
-        
+                    <div class="form-check form-switch admissionStatus">
+                        <label class="form-check-label" for="check${ students[i].id }">
+                            Admission Status: <span id="c-status${ students[i].id }">
+                                                ${ (students[i].admissionStatus)? 'Admitted' : 'Not Admitted' }
+                                            </span>
+                        </label>
+                        <input class="form-check-input" type="checkbox" id="check${ students[i].id }"
+                            onclick="setAdmissionStatus('${ students[i].id }')"
+                            ${ (students[i].admissionStatus)? 'checked' : '' }
+                        >
+                    </div>
+
+                    <div class="deleted">
+                        <button class="btn btn-danger" onclick="removeStudent('${students[i].id}')">Remove Student</button>
+                    </div>
+                </div>
+            </div>`
+        }
     }
     $('.students').append(_student);
     return _student;
@@ -88,12 +101,48 @@ function addNewStudent(){
             students.push(_newstudent);
             displayStudents();
         })
-    })
+    })   
+}
 
-    // $.post('https://students-26afb-default-rtdb.firebaseio.com/students.json', _student, function(_student){
-    //     students.push(_student);
-    //     displayStudents();
-    // })
-   
-    
+function setAdmissionStatus(id){
+    $.ajax({
+        accept: "application/json",
+        type: "PUT",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        url: "https://students-26afb-default-rtdb.firebaseio.com/students/" + id + "/admissionStatus.json",
+        data: JSON.stringify($('#check'+id).prop('checked'))
+    }).done(res => { 
+        $('#check'+id).prop('checked', res);
+        $('#c-status'+id).html(`${ (res)? 'Admitted' : 'Not Admitted' }`);
+    })
+}
+
+function removeStudent(id){
+    $.ajax({
+        accept: "application/json",
+        type: "PUT",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        url: "https://students-26afb-default-rtdb.firebaseio.com/students/" + id + "/deleted.json",
+        data: JSON.stringify(true)
+    }).done(res => { 
+        _index = students.map( (i) => { return i.id }).indexOf(id);
+        students[_index].deleted = true;
+        console.log('removing...');
+        $('#s'+id).fadeOut(1500)
+    })
+}
+
+function deleteStudent(id){
+    $.ajax({
+        accept: "application/json",
+        type: "DELETE",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        url: "https://students-26afb-default-rtdb.firebaseio.com/students/" + id + ".json",
+        data: JSON.stringify(true)
+    }).done(res => { 
+        console.log(res);
+    })
 }
